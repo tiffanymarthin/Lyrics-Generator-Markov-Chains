c08cfe908cd65791ac67c0893d2f8966b213018e
Tiffany Marthin <tiffanymarthin@gmail.com>
2021-03-03 12:38:19 -0800

    Initial commit and test requesting artist

---
+++ lyrics.py
@@ -0,0 +1,18 @@
+import configparser
+import requests
+
+# Gets token from config.ini file
+def getAccessToken():
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    return config['Client_Access_Token']['token']
+
+token = getAccessToken()
+
+def searchMusicArtist(name):
+    api_url = "https://api.genius.com/search?q={}".format(name)
+    headers = {"authorization": token}
+    r = requests.get(api_url, headers=headers)
+    return r.json()
+
+print(searchMusicArtist("sia"))
